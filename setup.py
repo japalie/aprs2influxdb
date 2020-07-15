914291c8e3ceabefe6297f9a43f9eb38ed25dffa
japalie <japa@japa.li>
2020-07-15 10:55:28 +0200

    Update setup.py
    Updated URL

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='aprs2influxdb',
-    version='0.2.1',
-    author='Bryce Salmi',
+    version='0.2.1.1',
+    author='Bryce Salmi, edit by Patrick Jakob',
     author_email='Bryce@FaradayRF.com',
     packages=['aprs2influxdb'],
-    url='https://github.com/FaradayRF/aprs2influxdb',
+    url='https://github.com/japalie/aprs2influxdb',
     license='GPLv3',
     description='Interfaces ham radio APRS-IS servers and saves packet data into an influxdb database',
     long_description=open('README.md').read(),
